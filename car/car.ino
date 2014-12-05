979f3ec03a22b1e726c603d9bc0ec2eaae8553ce
Costi <cgavrilescu@enovafinancial.com>
2014-12-05 13:50:22 -0600

    Adds IR remote codes

--- car/car.ino
+++ car/car.ino
@@ -1,5 +1,6 @@
 #include <FiniteStateMachine.h>
 #include <ClickButton.h>
+#include <IRremote.h>
 
 #define LEFT_ENABLE    11    // has to be PWM
 #define LEFT_BACKWARD  10
@@ -9,16 +10,26 @@
 #define RIGHT_FORWARD  7
 #define RIGHT_ENABLE   6  // has to be PWM
 
-#define START_BUTTON_PIN 2
+#define START_BUTTON_PIN 12
+
+#define IR_PIN 4
+
+#define IR_FORWARD   0x511DBB
+#define IR_BACKWARD  0xA3C8EDDB
+#define IR_RIGHT     0x52A3D41F
+#define IR_LEFT      0x52A3D41F
+#define IR_OK        0xD7E84B1B
 
 ClickButton startButton(START_BUTTON_PIN, LOW, CLICKBTN_PULLUP);
 
-#define NUMBER_OF_STATES 3
 State Forward = State(go_forward, noop, noop);
 State Backward = State(go_backward, noop, noop);
 State Stopped = State(stop_engines, noop, noop);
 
 FSM engineState = FSM(Stopped);
+
+IRrecv ir(IR_PIN);
+decode_results ir_results;
 
 byte buttonPresses = 0;
 
@@ -28,12 +39,18 @@ void setup(){
   pinMode(RIGHT_FORWARD, OUTPUT);
   pinMode(RIGHT_BACKWARD, OUTPUT);
 
+  ir.enableIRIn(); // start the receiver
+
   Serial.begin(9600);  //Begin serial communcation
   Serial.println("Start");
 }
 
 void loop(){
   startButton.Update();
+  if(ir.decode(&ir_results)){
+    Serial.println(ir_results.value, HEX);
+    ir.resume();
+  }
   analogWrite(LEFT_ENABLE, 200);
   analogWrite(RIGHT_ENABLE, 200);
   if(startButton.clicks != 0){
